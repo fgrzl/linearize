332256c578a41588647a3c22b6698bb8568b898e
smiggleworth <jrepanich@gmail.com>
2025-01-02 15:13:08 -0500

    move proto files

--- generate.go
+++ generate.go
@@ -1,6 +1,6 @@
 //go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 //go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
-//go:generate protoc --go_out=../ --go_opt=paths=source_relative --proto_path=./ linearize_models.proto
+//go:generate protoc --go_out=./ --go_opt=paths=source_relative --proto_path=./ linearize_models.proto
 
-package proto
+package linearize
