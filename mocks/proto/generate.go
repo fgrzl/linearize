bf716cd2f46aa41c58e6a2c419a2911ae955007e
smiggleworth <jrepanich@gmail.com>
2025-01-02 10:09:46 -0500

    confused

--- mocks/proto/generate.go
+++ mocks/proto/generate.go
@@ -1,3 +1,6 @@
+//go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+//go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
 //go:generate protoc --go_out=../ --go_opt=paths=source_relative --proto_path=./ mocks.proto
 
 package proto
