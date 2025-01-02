a0f9b2cf4c0223507977a8beef6eda66924badd7
smiggleworth <jrepanich@gmail.com>
2025-01-02 10:07:12 -0500

    path issues

--- mocks/proto/generate.go
+++ mocks/proto/generate.go
@@ -1,3 +1,3 @@
-//go:generate protoc --go_out=../ --go_opt=paths=source_relative --proto_path=./ *.proto
+//go:generate protoc --go_out=../ --go_opt=paths=source_relative --proto_path=./ mocks.proto
 
 package proto
